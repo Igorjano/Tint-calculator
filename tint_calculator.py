3459e9a895fcb2ab7ec353aaf63ec8d3616d7ded
Игорь Сычев <sychov13@gmail.com>
2024-03-11 00:33:19 +0200

    print_result_by_scales

--- tint_calculator.py
+++ tint_calculator.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import Radiobutton
 from tkinter import messagebox
-from collections import defaultdict
 
 
 class TintCalculator:
@@ -11,10 +10,11 @@ class TintCalculator:
         self.base_dose_entries = []
         self.tints_labels = []
         self.dose_labels = []
-        self.dose_labels_Y = []
+        self.dose_labels_by_scales = []
         self.tint_names = []
         self.result = []
-        self.result_y = []
+        self.result_by_scales = []
+        self.result_description_labels = []
         self.tint_amt = ('1', '2', '3', '4')
         self.tints_atr = {"WS": ["#666633", "#FFFFFF"], "XS": ["#000000", "#FFFFFF"], "JS": ["#000000", "#FFFFFF"],
                           "YS": ["#993300", "#FFFFFF"], "TS": ["#DAA520", "#FFFFFF"], "OS": ["#FFFFFF", "#000000"],
@@ -78,122 +78,156 @@ class TintCalculator:
         ValueError: If invalid input values are entered.
         """
 
+        self.create_result_description_labels()
+
         if self.result:
-            self.clear_labels(self.result)
+            self.result.clear()
+            self.result_by_scales.clear()
         try:
 
             for i in range(len(self.tints_entries)):
                 result = []
-                # tint_name = self.tints_entries[i].get().upper()
-                # self.tints_labels.append(tint_name)
                 res = round(float(self.base_dose_entries[i].get()) * float(self.dose_amt.get()), 2)
                 self.result.append(round(res, 2))
 
-                y_value, black_scale, red_scale = self.calculate_y(res)
+                y_value, black_scale, red_scale = self.calculate_scales_values(res)
 
                 result.append(y_value)
                 result.append(black_scale)
                 result.append(red_scale)
-                self.result_y.append(result)
+                self.result_by_scales.append(result)
 
         except ValueError:
             messagebox.showinfo("ERROR", "You enter wrong values!")
 
         self.ounce_384["state"] = "normal"
         self.ounce_Y["state"] = "normal"
-        self.print_ounce_384()
+
+        if self.ounce.get():
+            self.print_ounce_384()
+        else:
+            self.print_ounce_y()
 
     @staticmethod
-    def calculate_y(res):
+    def calculate_scales_values(res):
+        """
+
+        :param res:
+        :return:
+        """
+
         y = "--"
-        b = "--"
+        black = "--"
+        # In this case we have Y value in black scale
         if res >= 384:
             y = int(res // 384)
-            b = int(res % 384)
-            if b < 80:
-                r, b = b, "--"
+            black = int(res % 384)
+            if black < 80:
+                red, black = black, "--"
             else:
-                b = int((res % 384) // 8)
-                r = int(res % 8)
+                black = int((res % 384) // 8)
+                red = int(res % 8)
+        # We don't have Y values in black scale, only black scale values
         elif 384 > res >= 80:
-            b = int(res // 8)
-            r = int(res % 8)
+            black = int(res // 8)
+            red = int(res % 8)
+        # We have values only on red scale
         else:
-            r = int(res)
-        if b != "--" and b % 2 != 0:
-            b -= 1
-            r += 8
-        if r == 0:
-            r = "--"
+            red = int(res)
 
-        return y, b, r
+        if black != "--" and black % 2 != 0:
+            black -= 1
+            red += 8
+
+        if red == 0:
+            red = "--"
+
+        return y, black, red
+
+    def print_ounce_384(self):
+        if self.dose_labels_by_scales:
+            [self.clear_labels(label) for label in self.dose_labels_by_scales]
+            self.dose_labels_by_scales.clear()
+        self.print_tints()
+        self.print_ounce_384_label()
+
+        st = 270
+        for i in range(len(self.result)):
+            self.dose_labels.append(Label(root, text=self.result[i],
+                                          width=7,
+                                          justify=CENTER,
+                                          font=("Arial", 10)))
+            self.dose_labels[i].place(x=200, y=st)
+            st += 25
+
+    def print_ounce_y(self):
+        if self.dose_labels:
+            self.clear_labels(self.dose_labels)
+        self.print_tints()
+
+        st_y = 270
+        for i in range(len(self.result_by_scales)):
+            st_x = 170
+            self.dose_labels_by_scales.append([])
+            for j in range(len(self.result_by_scales[i])):
+                self.dose_labels_by_scales[i].append(Label(root, text=self.result_by_scales[i][j],
+                                                           width=7,
+                                                           justify=CENTER,
+                                                           font=("Arial", 10)))
+                self.dose_labels_by_scales[i][j].place(x=st_x, y=st_y)
+                st_x += 40
+            st_y += 25
+
+    def create_result_description_labels(self):
+        self.result_description_labels.append(Label(text="Y", font=("Times new roman bold", 10)))
+        self.result_description_labels.append(Label(text=48, font=("Times new roman", 10)))
+        self.result_description_labels.append(Label(text=384, font=("Times new roman", 11), fg='red'))
+
+    def print_ounce_384_label(self):
+        self.result_description_labels[2].place(x=213, y=240)
+
+    def print_description_ounce_by_labels(self):
+        pass
 
     def print_tints(self):
-        st = 250
+        st = 270
         if self.tints_labels:
             self.clear_result()
-        # try:
-        for i in range(len(self.tints_entries)):
-            tint_name = self.tints_entries[i].get().upper()
-            self.tints_labels.append(Label(root, text=tint_name,
-                                           justify=CENTER, width=5,
-                                           fg=self.tints_atr[tint_name][1], bg=self.tints_atr[tint_name][0]))
-            self.tints_labels[i].place(x=102, y=st)
-            st += 25
-        # except KeyError:
-        #     messagebox.showinfo("ERROR", "You enter wrong tint names!")
+
+        try:
+            for i in range(len(self.tints_entries)):
+                tint_name = self.tints_entries[i].get().upper()
+                self.tints_labels.append(Label(root, text=tint_name,
+                                               justify=CENTER,
+                                               width=5,
+                                               fg=self.tints_atr[tint_name][1],
+                                               bg=self.tints_atr[tint_name][0]))
+
+                self.tints_labels[i].place(x=102, y=st)
+                st += 25
+        except KeyError:
+            messagebox.showinfo("ERROR", "You enter wrong tint names!")
 
     def clear_result(self):
         self.clear_labels(self.tints_labels)
         self.clear_labels(self.dose_labels)
-        # self.clear_labels(self.dose_labels_y)
-
-    @staticmethod
-    def clear_labels(target):
-        for tint in target:
-            tint.destroy()
-        target.clear()
+        [self.clear_labels(labels) for labels in self.dose_labels_by_scales]
+        self.dose_labels_by_scales.clear()
 
     def clear_all(self):
-        self.tints_entries.clear()
-        self.base_dose_entries.clear()
         self.clear_result()
+        self.clear_labels(self.tints_entries)
+        self.clear_labels(self.base_dose_entries)
         self.dose_amt.delete(0, END)
         self.ounce_384["state"] = "disabled"
         self.ounce_Y["state"] = "disabled"
         self.tint_cbx["state"] = "enabled"
 
-    def print_ounce_384(self):
-        self.print_tints()
-        st = 250
-        for i in range(len(self.result)):
-            self.dose_labels.append(Label(root, text=self.result[i], width=7, justify=CENTER, font=("Arial", 10)))
-            self.dose_labels[i].place(x=200, y=st)
-            st += 25
-
-    def print_ounce_y(self):
-        self.clear_result()
-        self.print_tints()
-
-        st_x = 170
-        # result = list(self.result)
-        # print(result)
-        # print(len(result))
-        # for tint in range(len(self.tints_entries)):
-        #     tint_name = self.tints_entries[tint].get().upper()
-        #     self.dose_labels_y[i][tint] =
-
-        for i in range(len(self.result_y)):
-            st_y = 250
-            # print(values)
-            for j in range(len(self.result_y[i])):
-                self.dose_labels_Y[i].append(Label(root, text=self.result_y[i][j], width=7, justify=CENTER,
-                                                   font=("Arial", 10)))
-                print(self.dose_labels_y)
-                self.dose_labels_Y[i][j].place(x=st_x, y=st_y)
-
-                st_y += 25
-            st_x += 40
+    @staticmethod
+    def clear_labels(labels_list):
+        for label in labels_list:
+            label.destroy()
+        labels_list.clear()
 
 
 if __name__ == '__main__':
@@ -203,13 +237,13 @@ if __name__ == '__main__':
     root.iconbitmap('favicon.ico')
 
     WIDTH = 500
-    HEIGHT = 400
+    HEIGHT = 500
     POS_X = root.winfo_screenwidth() // 2 - WIDTH // 2
     POS_Y = root.winfo_screenheight() // 2 - HEIGHT // 2
     root.geometry(f"{WIDTH}x{HEIGHT}+{POS_X}+{POS_Y}")
 
     Label(text="Enter values for the calculation", font=("Times new roman", 12)).place(x=145, y=20)
-    Label(text="Tint           ", font=("Arial", 10)).place(x=70, y=60)
+    Label(text="Tints           ", font=("Arial", 10)).place(x=65, y=60)
     Label(text="Amount for 1 l(kg)", font=("Arial", 10)).place(x=190, y=60)
     Label(text="Dosage for           l(kg)", font=("Arial", 10)).place(x=180, y=210)
 
